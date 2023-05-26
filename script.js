958f571e0b26441218ed4a5848b751fef7beca87
SUSUNEKO <meaiyoutoo@gmail.com>
2023-05-26 13:42:27 +0900

    UPDATE
    フロント大体完成

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+// +++++++++blogList页面的设定+++++++
+  // DELETE BOTTON
+const deleteButton = document.getElementById("deleteButton");
+const confirmDialog = document.getElementById("confirmDialog");
+const noButton = document.getElementById("noButton");
+const yesButton = document.getElementById("yesButton");
 
+deleteButton.addEventListener("click", function() {
+  confirmDialog.style.display = "flex";
+});
+
+noButton.addEventListener("click", function() {
+  confirmDialog.style.display = "none";
+});
+
+yesButton.addEventListener("click", function() {
+  // 删除操作或其他逻辑?
+  confirmDialog.style.display = "none";
+});
+
+  
   // +++++++++++login.html++++++++++++++++
   function resizeInput() {
     var input = document.getElementById("myInput");
@@ -6,32 +26,9 @@
     input.style.height = (input.scrollHeight) + "px"; // 根据输入内容的行数调整高度
   }
   
-// ++++++++blog editor+++++++
-  document.addEventListener("DOMContentLoaded", function() {
-    // Set current date
-    var currentDate = new Date();
-    document.getElementById("current-date").textContent = currentDate.toLocaleDateString();
-  
-    // Initialize Quill editor
-    var editor = new Quill("#editor-container", {
-      theme: "snow"
-    });
-  
-    // Handle form submission
-    var uploadButton = document.querySelector(".upload-button");
-    uploadButton.addEventListener("click", function(event) {
-      event.preventDefault();
-      var title = document.getElementById("title").value;
-      var content = editor.root.innerHTML;
-      var currentDate = new Date();
-      var timestamp = currentDate.getTime(); // Generate timestamp or use any ID generation logic
-  
-      // Perform further processing or send data to backend
-      console.log("Title:", title);
-      console.log("Content:", content);
-      console.log("Timestamp:", timestamp);
-    });
-  });
 
-  // +++++++++blogList页面的设定+++++++
+  
+
+
+
  
